4677ef8e3614fcd0388c3a9d707e83ca35c5cc0d
Piotr Chełmicki <piotr.chelmicki@lppsa.con>
2017-06-08 09:48:09 +0200

    base configuration

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './start.js',
+    output: {
+        publicPath: "",
+        path: `${path.resolve(__dirname)}/dist/`,
+        filename: "[name].js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env']
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [new HtmlWebpackPlugin({ template: './src/index.html' })]
+};
