dee6e49c9cef7ca5cb5ef7d620c3fbcf327b635b
Piotr Chełmicki <piotr.chelmicki@lppsa.con>
2017-06-14 13:54:59 +0200

    add first element in middle of window

--- src/start.js
+++ src/start.js
@@ -1,19 +1,23 @@
 import Canvas from './canvas/Canvas';
 import roomfactory from './room/roomFactory';
 
+const HALF_START_WIDTH = 100;
+const HALF_START_HEIGHT = 50;
+const HALF_WINDOW_WIDTH = window.innerWidth / 2;
+const HALF_WINDOW_HEIGHT = window.innerHeight / 2;
+const P1 = { x: HALF_WINDOW_WIDTH - HALF_START_WIDTH, y: HALF_WINDOW_HEIGHT + HALF_START_HEIGHT };
+const P2 = { x: HALF_WINDOW_WIDTH + HALF_START_WIDTH, y: HALF_WINDOW_HEIGHT + HALF_START_HEIGHT };
+const P3 = { x: HALF_WINDOW_WIDTH + HALF_START_WIDTH, y: HALF_WINDOW_HEIGHT - HALF_START_HEIGHT };
+const P4 = { x: HALF_WINDOW_WIDTH - HALF_START_WIDTH, y: HALF_WINDOW_HEIGHT - HALF_START_HEIGHT };
 const canvas = new Canvas('#plan');
 canvas.fitToWindow();
 
+
 canvas.addElement(roomfactory()
-        .addPoint(2, 2)
-        .addPoint(100, 2)
-        .addPoint(100, 50)
-        .addPoint(2, 50))
-    .addElement(roomfactory()
-        .addPoint(110, 30)
-        .addPoint(250, 30)
-        .addPoint(250, 150)
-        .addPoint(110, 150))
+        .addPoint(P1.x, P1.y)
+        .addPoint(P2.x, P2.y)
+        .addPoint(P3.x, P3.y)
+        .addPoint(P4.x, P4.y))
     .draw();
 
 
